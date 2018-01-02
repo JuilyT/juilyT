d72ca90f0775a1725fee49cad6847ff6ffcb873d
juilyT <juily101291@gmail.com>
2018-01-02 16:54:48 +0530

    Routing changes

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -1,19 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes} from '@angular/router';
+import { LocationStrategy, HashLocationStrategy } from '@angular/common';
 
+import {HomeComponent} from './home.component';
+import {PageNotFoundComponent} from './pageNotFound.component'
 import { AppComponent } from './app.component';
 import { AppComponent2 } from './app.component2';
 import {AuthorComponent} from './app.author.component';
+import {AutoGrowDirective} from './auto-grow.directive';
 
+const appRoutes: Routes = [
+  {path:'home',component:HomeComponent},
+  {path:'authors',component:AuthorComponent},
+  {path:'',redirectTo:'home',pathMatch:'full'},
+  {path:'**', component:PageNotFoundComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent, AppComponent2, AuthorComponent
+    AppComponent, HomeComponent, PageNotFoundComponent, AppComponent2, AuthorComponent, AutoGrowDirective
   ],
   imports: [
-    BrowserModule
+    BrowserModule, RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ my-app/src/app/pageNotFound.component.ts
@@ -0,0 +1,8 @@
+import {Component} from "@angular/core"
+
+@Component({
+    template:'<h1>This is the page not found page</h1>'
+})
+export class PageNotFoundComponent{
+
+}
